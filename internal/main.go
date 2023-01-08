619693d6688efd3d4f4be82b23afdda2eeb28db3
Jesse <akohgodwinjesse@gmail.com>
2023-01-08 16:40:16 +0100

    fix

--- internal/main.go
+++ internal/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"log"
+	// "fmt"
 )
 
 const port string = ":4000"
@@ -11,9 +11,24 @@ const port string = ":4000"
 func main() {
 	http.HandleFunc("/", index)
 	log.Println("Listening on localhost ", port)
-	http.ListenAndServe(port, nil)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
 
 func index (w http.ResponseWriter, r *http.Request){
-   fmt.Fprintf(w , "Hello")
+	w.Header().Set("Content-Type", "application/json")
+
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		// fmt.Fprintf(w, "{message: '%q method requested'}", r.Method)
+		w.Write([]byte(`{message: "GET method requested"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{message: "POST method requested"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "Can't find method requested"}`))
+
+	}
+	
 }
